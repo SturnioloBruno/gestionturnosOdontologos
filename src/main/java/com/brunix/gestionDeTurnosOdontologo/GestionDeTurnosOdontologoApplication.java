5235a0912eb3001233a8fdf61315f7e68557b9f7
sturniolo <br1.sturniolo@gmail.com>
2023-12-19 12:56:57 -0300

    [GENESIS]configuracion de la BD y springboot

---
+++ src/main/java/com/brunix/gestionDeTurnosOdontologo/GestionDeTurnosOdontologoApplication.java
@@ -0,0 +1,13 @@
+package com.brunix.gestionDeTurnosOdontologo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDeTurnosOdontologoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeTurnosOdontologoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brunix/gestionDeTurnosOdontologo/GestionDeTurnosOdontologoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunix.gestionDeTurnosOdontologo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeTurnosOdontologoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
